f71ae8470fdd4f53bfeb8de7726764363708b57a
AliAngelinaaa <nanzum1140@gmail.com>
2023-06-14 14:15:43 -0400

    index.html

--- program.js
+++ program.js
@@ -1,20 +1,8 @@
-function check(password){
-    if(password!== 12345678){
-        console.log("ALERT: PASSWORD INCORRECT");
-    }else{
-        document.querySelector(h1).innerText("Changed");
+function check() {
+    let pass = document.getElementById("password").value;
+    if (pass != "12345678") {
+       console.log("ALERT: PASSWORD INCORRECT");
+    } else {
+       document.querySelector("h1").textContent = "Changed";
     }
-
-}
-
-
-/**
- * function check(){
- * let passwordInput = document.getElementById("password").value;
-    if(password!== 12345678){
-        console.log("ALERT: PASSWORD INCORRECT");
-    }else{
-        document.querySelector("h1").innerText = "Changed";
-    }
-}
- */
+ }
