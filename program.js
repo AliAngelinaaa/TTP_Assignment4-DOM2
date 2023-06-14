e206f99bfc26a806d4e575375018e1042d1490a3
AliAngelinaaa <nanzum1140@gmail.com>
2023-06-14 14:01:09 -0400

    fix js

--- program.js
+++ program.js
@@ -6,3 +6,15 @@ function check(password){
     }
 
 }
+
+
+/**
+ * function check(){
+ * let passwordInput = document.getElementById("password").value;
+    if(password!== 12345678){
+        console.log("ALERT: PASSWORD INCORRECT");
+    }else{
+        document.querySelector("h1").innerText = "Changed";
+    }
+}
+ */
