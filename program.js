2f052a9666fc947b890500dddf8b4d94da8f7b4c
rana <abedorana7@gmail.com>
2023-06-14 14:33:21 -0400

    pulling

--- program.js
+++ program.js
@@ -1,6 +1,6 @@
 function check() {
     let pass = document.getElementById("password").value;
-    if (pass != "12345678") {
+    if (pass !== "12345678") {
        console.log("ALERT: PASSWORD INCORRECT");
     } else {
        document.querySelector("h1").textContent = "Changed";
