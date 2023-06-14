765e87a90a1e8ce39aef4a594510edefb51f3344
rana <abedorana7@gmail.com>
2023-06-14 13:54:20 -0400

    form bug

---
+++ program.js
@@ -0,0 +1,8 @@
+function check(password){
+    if(password!== 12345678){
+        console.log("ALERT: PASSWORD INCORRECT");
+    }else{
+        document.querySelector(h1).innerText("Changed");
+    }
+
+}
